675533b04249ae380a1a2e14e9153555e802d452
June Kelly <june.kelly@overleaf.com>
2022-02-18 13:19:55 +0000

    even more basic

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-import { basicSetup, EditorState, EditorView } from '@codemirror/basic-setup';
-import { javascript } from '@codemirror/lang-javascript';
-import { keymap } from '@codemirror/view';
+import { EditorState } from '@codemirror/state';
+import { EditorView } from '@codemirror/view';
 
 console.log(">> init")
 
 const initialState = EditorState.create({
   doc: 'Hello there\n\nthis is nice.',
   extensions: [
-    basicSetup,
-    javascript(),
   ],
 });
 
