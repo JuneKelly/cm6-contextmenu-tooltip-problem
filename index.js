7e52d265232c0be4efe0fd946a6a7a3b3ebc2e6e
June Kelly <june.kelly@overleaf.com>
2022-02-18 13:42:22 +0000

    Replicated problem

--- index.js
+++ index.js
@@ -20,36 +20,50 @@ const messageTooltipField = StateField.define({
     for (const effect of tr.effects) {
       if (effect.is(showMessage)) {
         const message = effect.value
+        console.log("  - Show message for:", message)
         return {
           pos: 1,
           above: true,
           create: view => {
+            console.log("  - Create tooltip for:", message)
             return createMessageElement(message)
           }
         }
       }
     }
   },
-  provide: f => showTooltip.compute([f], state => state.field(f))
+  provide: f => showTooltip.compute([f], state => {
+    const value = state.field(f)
+    console.log("  - in showTooltip.compute(), with field value: ", value)
+    return value
+  })
 })
+
+let counter = 0
+
+const dispatchShowCounter = (view) => {
+  counter++
+  view.dispatch({
+    effects: [
+      showMessage.of(`Count: ${counter}`)
+    ]
+  })
+}
 
 const initialState = EditorState.create({
   doc: 'Hello there\n\nthis is nice.\n\nTest one two\n\nThree four.',
   extensions: [
     EditorView.domEventHandlers({
       contextmenu: (event, view) => {
-        console.log(">> contextMenu")
+        console.log(">>>>>>>>>> contextmenu event <<<<<<<<<<<<<<")
         event.preventDefault()
-        view.dispatch({
-          effects: [
-            showMessage.of(`Time: ${new Date()}`)
-          ]
-        })
+        dispatchShowCounter(view)
       }
     }),
     messageTooltipField
   ],
 });
+
 
 const view = new EditorView({
   parent: document.getElementById('editor'),
